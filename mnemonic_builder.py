f0be384191d1fefa84e57b0e4f4c5b22fd21db1b
Hugh Brown <hughdbrown@yahoo.com>
2009-06-21 17:29:05 -0400

    Version that generates a list rather than just the last high scorer encountered

--- mnemonic_builder.py
+++ mnemonic_builder.py
@@ -46,10 +46,15 @@ w = create_word_candidates()
 
 for k in d.keys() :
     dk = create_collections_dict(k)
-    max_score = 0
+    max_score = -1
     match = None
     for kk in w.keys():
         x = score(dk, kk)
         if x > max_score :
-            max_score, match = x, kk
-    print k, d[k], max_score, match, w[match]
+            max_score, match = x, [ kk ]
+        elif x == max_score:
+            match.append(kk)
+    if max_score >= 9:
+        print k, d[k], max_score
+        for m in sorted(match):
+            print "\t", m, w[m]
