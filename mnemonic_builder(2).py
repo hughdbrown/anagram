8a4b495b1fd7879a92f5b0b058c074d9b58523a2
Hugh Brown <hughdbrown@yahoo.com>
2009-06-22 13:03:24 -0400

    The obvious refactoring: move shared code to single location

--- mnemonic_builder(2).py
+++ mnemonic_builder(2).py
@@ -1,58 +1,11 @@
-from __future__ import with_statement
-import collections
-
-FIRST_FIT = 5
-SECOND_FIT = 10 - FIRST_FIT
-
-def create_key (lettersets, sofar) :
-    if len(lettersets) == 0 :
-        yield sofar
-    else :
-        letterset, leftover = lettersets[0], lettersets[1 :]
-        for letter in letterset :
-            for key in create_key(leftover, sofar + letter) :
-                yield key
-    return
-
-def create_word_candidates():
-    filename = r'g:\words\words(3).txt'
-    with open(filename) as f:
-        candidates = [ str.strip() for str in f.readlines() if len(str.strip()) in (FIRST_FIT, SECOND_FIT)]
-    
-    w = collections.defaultdict(list)
-    for c in candidates:
-        key = c.upper()
-        sorted_key = "".join(sorted(key))
-        w[sorted_key].append(key)
-    return w
-
-def create_mnemonic_candidates():    
-    mnemonics = [ "IM", "E", "SCN", "EGI", "CH", "E", "S", "T", "F", "T" ]
-    d = collections.defaultdict(list)
-    for key in create_key(mnemonics, ""):
-        sorted_key = "".join(sorted(key))
-        d[sorted_key].append(key)
-    return d
-
-def create_collections_dict(key):
-    dk = collections.defaultdict(int)
-    for k in key:
-        dk[k] += 1
-    return dk
-
-def score(dk, cand) :
-    dc = create_collections_dict(cand)
-    return sum(min(dk[k], dc[k]) for k in dk.keys() if k in dc)
-    
-def diff(long_key, short_key) :
-    ld = create_collections_dict(long_key)
-    sd = create_collections_dict(short_key)
-    s = [ (k * (ld[k] - (sd[k] if k in sd else 0))) for k in ld.keys() ]
-    return "".join(sorted(s))
+from mnemonic_util import *
 
 if __name__ == "__main__" :
+    FIRST_FIT = 5
+    SECOND_FIT = 10 - FIRST_FIT
+    wordlens = (FIRST_FIT, SECOND_FIT)
     d = create_mnemonic_candidates()
-    w = create_word_candidates()
+    w = create_word_candidates2(wordlens)
     
     for k in d.keys() :
         dk = create_collections_dict(k)
@@ -61,6 +14,5 @@ if __name__ == "__main__" :
             x = score(dk, kk)
             if x == FIRST_FIT :
                 kk2 = diff(k, kk)
-                #if kk2 in w and (kk < kk2) :
                 if kk2 in w and score(dk, kk2) == SECOND_FIT :
                     print k, kk, kk2, w[kk], w[kk2]
